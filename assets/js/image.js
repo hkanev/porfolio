60f8f7c50ab3a5f4ee8fd25afaef186921012176
Hristian Kanev <hristiankanev@gmail.com>
2017-12-30 13:36:46 +0200

    Added image function
    Added image function to move next and previous picture.

---
+++ assets/js/image.js
@@ -0,0 +1,39 @@
+(() => {
+    let images = ['cert3', 'cert4', 'cert1', 'cert2', 'cert5', 'cert6'];
+    let mainImage = $('#certificateImage' );
+
+    $('#nextBtn').click((e) => {
+        e.preventDefault();
+        mainImage.fadeOut(100,function()
+        {
+            let next_val = document.getElementById( "img_no" ).value;
+            next_val = Number(next_val)+1;
+            if(next_val >= images.length)
+            {
+                next_val = 0;
+            }
+            mainImage.attr( 'src' , 'assets/images/'+images[next_val]+'.jpg' );
+            document.getElementById( "img_no" ).value = next_val;
+        });
+        mainImage.fadeIn(1000);
+    });
+
+    $('#previousBtn').click((e) => {
+        e.preventDefault();
+        mainImage.fadeOut(100,function()
+        {
+            let prev_val = document.getElementById( "img_no" ).value;
+            prev_val = Number(prev_val)-1;
+            if(prev_val <= -1)
+            {
+                prev_val = images.length - 1;
+            }
+            mainImage.attr( 'src' , 'assets/images/'+images[prev_val]+'.jpg' );
+            document.getElementById( "img_no" ).value = prev_val;
+        });
+        mainImage.fadeIn(1000);
+    });
+
+
+})();
+
